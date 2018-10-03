1b17cc6008c4cc9403adfbb6030cccd4861e88ee
Michael Perry <lmperry@stanford.edu>
2018-10-03 09:35:57 -0700

    repo modifications

--- run_docker.sh
+++ run_docker.sh
@@ -3,4 +3,4 @@ docker run --rm -it \
 		-v /black/localhome/glerma/soft/dwi-flip-bvec/config.json:/flywheel/v0/config.json \
         -v /black/localhome/glerma/TESTDATA/flipbvec/input:/flywheel/v0/input \
         -v /black/localhome/glerma/TESTDATA/flipbvec/output:/flywheel/v0/output \
-        scitran/dwi-flip-bvec:1.0.0
+        vistalab/dwi-flip-bvec:1.0.0
